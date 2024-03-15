cf78668ca926a239668872212ea5b779156122ab
kirdol <lodrik.adam@hotmail.ch>
2024-03-15 13:08:53 +0100

    up

---
+++ scripts/Lodrik.R
@@ -0,0 +1,9 @@
+data_selected <- data |>
+  select(Serial2, F05_01.ENG, F05_02, F05_03, F05_04, F12, F13, F15.ENG,
+         F16.ENG, F17, F20, F21, F30.ENG, F31_01_ENG, F31_02_ENG, F31_03_ENG,
+         F31_04_ENG, F31_05_ENG, F31_06_ENG, F31_07_ENG, F31_08_ENG, F31_09,
+         F32, F33, hFerienDauer, hGruppePersonen, F80, F80_a05, F81_01, F81_02,
+         F81_03, F81_04, F81_05, F81_06, F81_Total, F82_01, F82_02, F82_03,
+         F82_04, F82_05, F82_06, F82_07, F82_Total, F83, F85, F105, F105A,
+         F105_Code, F105_Code_nr, F105_OrgCode, F125_ENG, F125_a11.ENG, F30,
+         F130_a07, F135_01, F12_F13_Mittelwert_2Cat_final)
