bcc7dfe8ec5c8f430f1396f9870b209622d390c9
Victor Hogan <35497878+vh6@users.noreply.github.com>
2024-04-02 15:43:59 +0200

    update carte leaflet

--- scripts/cleaning_victor.R
+++ scripts/cleaning_victor.R
@@ -45,3 +45,44 @@ ggplot(df_sums_df, aes(x = Column, y = Percentage)) +
   geom_bar(stat = "identity", fill = "skyblue") +
   geom_bar(data = subset(df_sums_df, Column == "couples"), fill = "orange", stat = "identity") +
   labs(title = "Frequency of responses", x = "Column", y = "Percentage")
+
+
+#######################################################################################################################################
+# Leaflet map
+
+library(leaflet)
+library(sf)
+
+
+districts <- read_sf("data/swiss_districts.geojson")
+districts
+
+vaud <- districts |>
+  filter(KANTONSNUM == 22)
+
+# We will transform data to include corresponding leaflet district. 
+unique(data$F105_Code)
+
+# Create a new column 'leaflet_region' with NA values
+data$leaflet_districts <- NA
+
+# Assign cities to districts based on conditions
+data$leaflet_districts[data$F105_Code == "Lausanne"] <- "Lausanne"
+data$leaflet_districts[data$F105_Code %in% c("Montreux", "Vevey", "La Tour-de-Peilz", "Rougemont", "Château-d'Oex", "Veytaux", "Rossinière", "Corsier-sur-Vevey")] <- "Riviera-Pays-d'Enhaut"
+data$leaflet_districts[data$F105_Code %in% c("Morges", "Yens", "Aubonne")] <- "Morges"
+data$leaflet_districts[data$F105_Code %in% c("Yverdon-les-Bains", "L'Abbaye", "Vallorbe", "Orbe", "Yvonand", "La Praz", "Le Chenit", "Bullet", "Montagny-près-Yverdon")] <- "Jura-Nord vaudois"
+data$leaflet_districts[data$F105_Code %in% c("Aigle", "Leysin", "Villeneuve (VD)", "Ormont-Dessus", "Bex", "Lavey-Morcles", "Gryon", "Ormont-Dessous")] <- "Aigle"
+data$leaflet_districts[data$F105_Code %in% c("Nyon", "Prangins", "Chavannes-de-Bogis", "Coppet", "Rolle", "Commugny", "Duillier", "Gilly", "Founex")] <- "Nyon"
+data$leaflet_districts[data$F105_Code %in% c("Valbroye", "Lucens", "Avenches", "Payerne", "Cudrefin")] <- "Broye-Vully"
+data$leaflet_districts[data$F105_Code %in% c("Crissier", "Bussigny", "Ecublens (VD)")] <- "Ouest lausannois"
+data$leaflet_districts[data$F105_Code %in% c("Chexbres", "Bourg-en-Lavaux")] <- "Lavaux-Oron"
+data$leaflet_districts[data$F105_Code %in% c("Echallens")] <- "Gros-de-Vaud"
+
+
+# Transform to leaflet projection if needed
+vaud <- st_transform(vaud, crs = '+proj=longlat +datum=WGS84')
+
+leaflet() %>%
+  addTiles() %>%
+  setView(lng = 6.63, lat = 46.51, zoom = 9) %>%
+  addPolygons(data = vaud, stroke = 1, opacity = 0.8)
